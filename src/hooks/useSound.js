baee5ca365dfdc5d3a34f71dacc743e399f92a83
wwesleym <wesleyma007@gmail.com>
2026-01-08 14:26:16 -0800

    Fix mobile sound effects

--- src/hooks/useSound.js
+++ src/hooks/useSound.js
@@ -1,33 +1,46 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 let audioCtx;
 
+// creates audio context which gets reused
 function getAudioContext() {
-  if (!audioCtx) {
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  }
-  return audioCtx;
+    if (!audioCtx) {
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    }
+    return audioCtx;
 }
 
 export default function useSound(src, enabled = true) {
-  const audioRef = useRef(null);
+    const bufferRef = useRef(null);
 
-  if (!audioRef.current) {
-    audioRef.current = new Audio(src);
-    audioRef.current.preload = "auto";
-  }
+    // load and decode sound per sound file
+    useEffect(() => {
+        const loadSound = async () => {
+            const ctx = getAudioContext();
 
-  return async () => {
-    if (!enabled) return;
+            const res = await fetch(src);
+            const arrayBuffer = await res.arrayBuffer();
+            bufferRef.current = await ctx.decodeAudioData(arrayBuffer);
+        };
 
-    const ctx = getAudioContext();
+        loadSound();
+    }, [src]);
 
-    // REQUIRED for mobile
-    if (ctx.state === "suspended") {
-      await ctx.resume();
-    }
+    return async () => {
+        // stops playback if sound is muted or sound is not loaded
+        if (!enabled || !bufferRef.current) return;
 
-    audioRef.current.currentTime = 0;
-    audioRef.current.play().catch(() => {});
-  };
+        const ctx = getAudioContext();
+
+        // required for mobile
+        if (ctx.state === "suspended") {
+            await ctx.resume();
+        }
+
+        // create new source for every sound
+        const source = ctx.createBufferSource();
+        source.buffer = bufferRef.current;
+        source.connect(ctx.destination);
+        source.start(0);
+    };
 }
